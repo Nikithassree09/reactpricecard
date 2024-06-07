9d9409e026812efec0d0b65900b7ab949ea7e42f
Nikithassree09 <157340744+Nikithassree09@users.noreply.github.com>
2024-06-07 12:55:29 +0530

    Delete Documents/FSDprojectFrontend/Gasbooking directory

--- Documents/FSDprojectFrontend/Gasbooking/readme.md
+++
@@ -1,3 +0,0 @@
-# Frontend Application Repository
-
-This repository contains the backend application for the [Frontend Application Repository]().
--- Documents/FSDprojectFrontend/Gasbooking/src/services/Gascompanyservices.jsx
+++
@@ -1,8 +0,0 @@
-import { protectedInstance } from "./instance";
-
-const Gascompanyservices = {
-    getgasCompanies: async () => {
-        return await protectedInstance.get('/gascompanies');
-    }
-}
-export default Gascompanyservices;
